c0afcbd2748e69dd57d69f993084c5939f2f1b5a
billbo99 <cactus_computers-github@yahoo.com>
2020-08-26 15:03:26 +0100

    1.0.1

---
+++ dist/control.lua
@@ -0,0 +1,28 @@
+local Items = require("migrations.items").items
+local Func = require("utils.func")
+
+script.on_configuration_changed(
+    function(e)
+        -- e.mod_changes
+        -- e.mod_startup_settings_changed
+        -- e.migration_applied
+        log("on_configuration_changed")
+        if e.mod_startup_settings_changed or e.mod_changes["DeadlockStackingForVanilla"] then
+            for index, force in pairs(game.forces) do
+                local recipes = force.recipes
+
+                for _, item in pairs(Items) do
+                    if force.technologies[item.tier] and force.technologies[item.tier].researched then
+                        local tech = force.technologies[item.tier]
+                        for _, effect in pairs(tech.effects) do
+                            if effect.type == "unlock-recipe" and Func.starts_with(effect.recipe, "deadlock") then
+                                recipes[effect.recipe].enabled = true
+                                recipes[effect.recipe].reload()
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+)
